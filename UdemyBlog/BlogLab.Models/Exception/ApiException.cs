cecc94dc50e99862556b53f35f2b204f8ee593d5
OrSp <orestisSp@outlook.com>
2021-08-25 00:05:04 +0300

    Models

---
+++ UdemyBlog/BlogLab.Models/Exception/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace BlogLab.Models.Exception
+{
+    public class ApiException
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
