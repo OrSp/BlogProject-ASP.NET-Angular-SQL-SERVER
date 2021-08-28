3b0dd3d9ddfb1b3ebd94f5fdb242e1a6d302e094
OrSp <orestisSp@outlook.com>
2021-08-28 23:20:07 +0300

    Completed repositories

--- UdemyBlog/BlogLab.Models/Blog/PagedResults.cs
+++ UdemyBlog/BlogLab.Models/Blog/PagedResults.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlogLab.Models.Blog
 {
-    public class PageResults<T>
+    public class PagedResults<T>
     {
         public IEnumerable<T> Items { get; set; }
 
